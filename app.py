e7901278972706684c968a88153b5e0a32e7f79c
BartekMazurek <bartlomiejmazurek9@gmail.com>
2023-01-02 14:10:30 +0100

    Added JWT configuration

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 import models
 
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
@@ -23,12 +26,39 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
+    # HANDLE DATABASE MIGRATIONS
+    migrate = Migrate(app, db)
+
     api = Api(app)
 
-    with app.app_context():
-        db.create_all()
+    # JWT CONFIGURATION
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+    jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "Token has expired"}), 401
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify({"message": "Invalid signature"}), 401
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify({"message": "Missing access token"}), 401
+        )
+
+    # USED BEFORE MIGRATION IMPLEMENTATION
+    # with app.app_context():
+    #     db.create_all()
 
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
